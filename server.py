b128e6d70d15b8128656252fb182e717951758b0
Fier <alan.sanchezram@alumno.buap.mx>
2026-03-25 23:00:57 -0600

    corregir error de despliegue

--- server.py
+++ server.py
@@ -29,7 +29,7 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def serve_home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(request=request, name="index.html")
 
 @app.get("/api/ciudades/{estado_id}")
 async def get_ciudades(estado_id: str):
